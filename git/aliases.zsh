6fafdaf42e5dca9adf3463016d22f1cfa6734a95
Mateus Junges <mateus@junges.dev>
2023-01-30 00:25:26 +0000

    Add nah

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,4 @@
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 alias amend='git commit --amend'
+alias nah="git reset --hard && git clean -df"
